c7ee417e52b7f54fec163ea34a62c537346eaefc
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-21 14:29:24 -0800

    Added Key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD46yIObpdOu8ai+UfQQ4u61HCPoKelljevily/fvnz41KF3He8JUiFHc3brirmQHtD4XeQnzwWxj3MPwVjyjmn13v71N/EOQX3jIoxVeZ+GhJx87rFGKAEVmf2I58aISmayXGJVo1qHsSNAM0Lrx+WTBu5W95FvHR5FNYEewcTFUt3YaYLdgzXytCUivFsaRgkAwO/HgTIqNEcW+cwiX5EWQQ2p+CU6b9CHXodW6xBH25XqfhWHlSHl7Hlius3hXkePtUQWauW2E/hBFUDW4Fg5wktCuQ4Hpoa/9novOXFrZIaea68l9YMT65laCWZ0PylHKS9fS7Fky3IruFnqx5F'
   }  
 }
