a1d459e81f8c72b38d3096b5da4b03702e4b4559
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-20 21:43:11 -0800

    Adding Facts

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,19 @@
 node default {
-  file 
-  { '/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root'
-  }
+#  file 
+#  { '/root/README':
+#    ensure  => file,
+#    content => 'This is a readme',
+#    owner   => 'root'
+#  }
 }
 
 node 'ubuntu1804.localdomain' {
   include role::master_server
+  file 
+  { '/root/README':
+    ensure  => file,
+    content => $fqdn
+  }
 }
 
 node /^web/ {
