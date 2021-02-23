c1d7dc8369b858cabf5cbb94e31aa21763b2eda4
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-23 05:19:26 -0800

    Commenting

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,14 +3,14 @@ class profile::agent_nodes {
   dockeragent::node {"web.puppet.vm":}
   dockeragent::node {"db.puppet.vm":}
   dockeragent::node {"minetest.puppet.vm":}
-  host
-  {'web.puppet.vm'
-   ensure => present,
-   ip     => '10.0.2.15'
-  }
-  host
-  {'db.puppet.vm'
-   ensure => present,
-   ip     => '10.0.2.15'
-  }
+#  host
+#  {'web.puppet.vm'
+#   ensure => present,
+#   ip     => '10.0.2.15'
+#  }
+#  host
+#  {'db.puppet.vm'
+#   ensure => present,
+#   ip     => '10.0.2.15'
+#  }
 }
