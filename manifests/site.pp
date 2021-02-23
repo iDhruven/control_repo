b821530eee9e486d951b3b8867b231e3fce299de
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-23 13:25:35 -0800

    Added minetest node

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,7 +15,9 @@ node 'ubuntu1804.localdomain' {
     content => "Welcome to ${fqdn}\n"
   }
 }
-
+node 'minetest.puppet.vm'{
+  include role::minecraft_server
+  }
 node /^web/ {
   include role::app_server
 }
