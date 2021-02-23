0113d1dbfbc64df82eee604a656c5daf335f5375
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-23 03:07:43 -0800

    Update Key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD46yIObpdOu8ai+UfQQ4u61HCPoKelljevily/fvnz41KF3He8JUiFHc3brirmQHtD4XeQnzwWxj3MPwVjyjmn13v71N/EOQX3jIoxVeZ+GhJx87rFGKAEVmf2I58aISmayXGJVo1qHsSNAM0Lrx+WTBu5W95FvHR5FNYEewcTFUt3YaYLdgzXytCUivFsaRgkAwO/HgTIqNEcW+cwiX5EWQQ2p+CU6b9CHXodW6xBH25XqfhWHlSHl7Hlius3hXkePtUQWauW2E/hBFUDW4Fg5wktCuQ4Hpoa/9novOXFrZIaea68l9YMT65laCWZ0PylHKS9fS7Fky3IruFnqx5F'
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCwqGSFRSCvUi4wGlYH+Brhyu2VpktAqVk8MSMoZ4lX+vIjNyXXQjDfySQ8zygiB6ztOi2qNJEsxLZRPrcNOrK+ET0LIfsDoWrjBNRZD2g35i9YYsQpHogUnPGxiMV0XlEemWzlUQmDvv6FAt3vG4p2UqExXfVp+AogSuBbT3qFVFiPvX/2ksDGiL8j07ESS90vync9Q9hPpbEx0k6yi4P9Chh0oO7Rfu1qHwLQdXj018rmht+mHNl1B7BhQ0k7uFvRzv8ZQEodhq/8wfp3rIpUpbluf96xpLiS9yDKviFfrYzFjJYyK8y+K/tyK2VQVOpJQTOcxOLzNTHXlaASdFS5'
   }  
 }
