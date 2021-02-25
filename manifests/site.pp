94e8776eada2252993376accaf21a30793f4a888
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-24 23:04:23 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,10 @@
 node default {
- # file 
- # { '/root/README':
- #   ensure  => file,
- #   content => 'This is a readme',
- #   owner   => 'root'
- # }
+#  file 
+#  { '/root/README':
+#    ensure  => file,
+#    content => 'This is a readme',
+#    owner   => 'root'
+#  }
 #}
 
 #node 'ubuntu1804.localdomain' {
