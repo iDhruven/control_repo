6cbfc1fd44bc693bb071f645cf90f77f9fe311f2
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-24 22:40:33 -0800

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,13 +1,13 @@
 class profile::r10k {
-  class {'r10k':
-    remote => 'https://github.com/iDhruven/control_repo'
-  }
-  class {'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl      => false
-  }
-  class {'r10k::webhook':
-    user  => 'root',
-    group => 'root'
-  }
+#  class {'r10k':
+#    remote => 'https://github.com/iDhruven/control_repo'
+#  }
+#  class {'r10k::webhook::config':
+#    use_mcollective => false,
+#    enable_ssl      => false
+#  }
+#  class {'r10k::webhook':
+#    user  => 'root',
+#    group => 'root'
+#  }
 }
