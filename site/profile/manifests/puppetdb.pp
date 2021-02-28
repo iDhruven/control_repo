a7cf80b183d78282eb5211bfdeea4eafce0a6fa8
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-27 21:32:12 -0800

    Update puppetdb.pp

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -2,5 +2,5 @@ class profile::puppetdb{
    # Configure puppetdb and its underlying database
   class { 'puppetdb': }
   # Configure the Puppet master to use puppetdb
-  class { 'puppetdb::master::config': }
+  class { 'puppetdb::master_server::config': }
 }
