b2ffb3f62ceba304fc622d9fd0bec022ffabbbfd
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-02-27 20:39:30 -0800

    Commenting docker

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,8 +1,8 @@
 class profile::agent_nodes {
-  include dockeragent
-  dockeragent::node {"web.puppet.vm":}
-  dockeragent::node {"db.puppet.vm":}
-  dockeragent::node {"minetest.puppet.vm":}
+#  include dockeragent
+#  dockeragent::node {"web.puppet.vm":}
+#  dockeragent::node {"db.puppet.vm":}
+#  dockeragent::node {"minetest.puppet.vm":}
 #  host
 #  {'web.puppet.vm'
 #   ensure => present,
